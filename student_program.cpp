a3de264efb57cc03b9f165d8e32d9e4ebe80f0fd
Adsadawut Siriluk <lolnnani005@gmail.com>
2025-01-29 11:55:34 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+char* reverse( char str1[]) ;
+
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
+    char text[ 50 ] ;
+    char *output ;
+    output = reverse( text ) ;
+    printf( "MAIN : %s\n", output ) ;
 }//end function
+
+char* reverse( char str1[]) {
+    printf( "Enter str1 : " ) ;
+    fgets(str1 , 50 , stdin) ;
+    str1[strcspn(str1 , "\n" )] = '\0' ; 
+
+    int len = strlen(str1) ;
+
+    for (int i = 0 , j = len - 1 ; i < j ; i++ , j--) {
+        char temp = str1[i] ;
+        str1[i] = str1[j] ;
+        str1[j] = temp ;
+    }
+
+    printf( "FUNC : %s\n" , str1 ) ;
+
+    return str1 ;
+}
